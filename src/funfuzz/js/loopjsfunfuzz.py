06c56cb9ab6655b098114a1da37ba8c4be204fc0
Gary Kwong <gary@rumblingedge.com>
2017-08-25 17:23:03 -0700

    Rename buildOptions to build_options.

--- src/funfuzz/js/loopjsfunfuzz.py
+++ src/funfuzz/js/loopjsfunfuzz.py
@@ -47,7 +47,7 @@ def parseOpts(args):
                       default=os.path.expanduser("~/trees/mozilla-central/"),
                       help="The hg repository (e.g. ~/trees/mozilla-central/), for bisection")
     parser.add_option("--build",
-                      action="store", dest="buildOptionsStr",
+                      action="store", dest="build_options_str",
                       help="The build options, for bisection",
                       default=None)  # if you run loopjsfunfuzz.py directly without --build, pinpoint will try to guess
     parser.add_option("--valgrind",
@@ -181,7 +181,7 @@ def many_timed_runs(targetTime, wtmpDir, args, collector):
             itest.append(options.knownPath)
             (lithResult, _lithDetails, autoBisectLog) = pinpoint.pinpoint(
                 itest, logPrefix, options.jsEngine, engineFlags, filenameToReduce, options.repo,
-                options.buildOptionsStr, targetTime, res.lev)
+                options.build_options_str, targetTime, res.lev)
 
             # Upload with final output
             if lithResult == lithOps.LITH_FINISHED:
@@ -213,7 +213,7 @@ def many_timed_runs(targetTime, wtmpDir, args, collector):
                 fileManipulation.writeLinesToFile(linesToCompare, jitcomparefilename)
                 anyBug = compareJIT.compareJIT(options.jsEngine, engineFlags, jitcomparefilename,
                                                logPrefix + "-cj", options.repo,
-                                               options.buildOptionsStr, targetTime, jsInterestingOptions)
+                                               options.build_options_str, targetTime, jsInterestingOptions)
                 if not anyBug:
                     os.remove(jitcomparefilename)
 
