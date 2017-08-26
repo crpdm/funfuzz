d62fbad3edc965c6eba0afe9e594151ac90450c7
Gary Kwong <gary@rumblingedge.com>
2017-08-25 17:34:04 -0700

    Rename compareJIT to compare_jit.

--- src/funfuzz/js/__init__.py
+++ src/funfuzz/js/__init__.py
@@ -10,7 +10,7 @@
 from __future__ import absolute_import
 
 from . import build_options
-from . import compareJIT
+from . import compare_jit
 from . import compileShell
 from . import detect_malloc_errors
 from . import inspectShell
